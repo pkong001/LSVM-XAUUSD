e105acce3350190ba9d8bf929c0fedb3b5c03c34
pkong001 <pkongdan01@gmail.com>
2023-04-12 07:22:47 +0700

    none'

--- run_server_and_bot.bat
+++ run_server_and_bot.bat
@@ -1,4 +1,5 @@
 #echo off
 call start cmd /k "C:\Users\pkong\LSVM-XAUUSD\server_run.bat"
 call start cmd /k "C:\Users\pkong\Trading-bot-lsvm\bot_run.bat"
+call start cmd /k "C:\Users\pkong\Trading-bot-lsvm-v2\bot_run_v2.bat"
 exit
