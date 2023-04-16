35d9568bdc915c53617f9f4e1b9cc7d3e85234ae
pkong001 <pkongdan01@gmail.com>
2023-04-16 22:58:28 +0700

    Add model for botv2b

--- run_server_and_bot.bat
+++ run_server_and_bot.bat
@@ -2,4 +2,5 @@
 call start cmd /k "C:\Users\pkong\LSVM-XAUUSD\server_run.bat"
 call start cmd /k "C:\Users\pkong\Trading-bot-lsvm\bot_run.bat"
 call start cmd /k "C:\Users\pkong\Trading-bot-lsvm-v2\bot_run_v2.bat"
+call start cmd /k "C:\Users\pkong\Trading-bot-lsvm-v2\bot_run_v2b.bat"
 exit
