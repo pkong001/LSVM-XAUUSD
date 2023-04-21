77c98eefbd7edda4d968932fe27ef725a200e3ee
pkong001 <pkongdan01@gmail.com>
2023-04-21 15:35:46 +0700

    Rearrage folder, begin new classification model

--- run_server_and_bot.bat
+++ run_server_and_bot.bat
@@ -3,4 +3,5 @@ call start cmd /k "C:\Users\pkong\LSVM-XAUUSD\server_run.bat"
 call start cmd /k "C:\Users\pkong\Trading-bot-lsvm\bot_run.bat"
 call start cmd /k "C:\Users\pkong\Trading-bot-lsvm-v2\bot_run_v2.bat"
 call start cmd /k "C:\Users\pkong\Trading-bot-lsvm-v2\bot_run_v2b.bat"
+call start cmd /k "C:\Users\pkong\Trading-bot-lsvm-v2\bot_run_v2c.bat"
 exit
